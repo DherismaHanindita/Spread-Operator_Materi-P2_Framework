7064894d701efd1f543354d3840754f286f6273e
= <=>
2023-02-26 13:42:11 +0700

    Spread Operator

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // SPREAD OPERATOR
-// memecah iterable menjadi single element
-
+//1. memecah iterable menjadi single element
 // const mhs = ['Dherisma', 'Izza', 'Faiza'];
-// console.log(...mhs[0]); 
+// console.log(...mhs); 
+// ======================================================
 
-// kita menggunakan Spread operator yaitu
-
-// menggabungkan 2 array
-
+//2. Menggabungkan 2 array
 // const mhs = ['Dherisma', 'Izza', 'Faiza'];
 // const dosen = ['Prima', 'Rosa', 'Ando'];
 // const orang = [...mhs, ...dosen];
 // console.log(orang); 
+// ======================================================
 
+// 3. Menggabungkan elemen array lain ditengan
+// const mhs = ['Dherisma', 'Izza', 'Faiza'];
+// const dosen = ['Prima', 'Rosa', 'Ando'];
+// const orang = [...mhs, 'Hanindita', ...dosen];
+// console.log(orang); 
+// ======================================================
 
-// Melalkukakn copy dalam array
-
+// 4. Menyalin array atau objek
 const mhs = ['Dherisma', 'Izza', 'Faiza'];
+const mhs1 = [...mhs];
+mhs1[0] = 'Hanindita';
+console.log(mhs1);
