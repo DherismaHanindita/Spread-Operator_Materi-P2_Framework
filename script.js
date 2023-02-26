7e2a887ee7cace30f6d709f5cdb7d6f1b656ce2d
= <=>
2023-02-26 12:41:00 +0700

    Spread Operator

---
+++ script.js
@@ -0,0 +1,19 @@
+// SPREAD OPERATOR
+// memecah iterable menjadi single element
+
+// const mhs = ['Dherisma', 'Izza', 'Faiza'];
+// console.log(...mhs[0]); 
+
+// kita menggunakan Spread operator yaitu
+
+// menggabungkan 2 array
+
+// const mhs = ['Dherisma', 'Izza', 'Faiza'];
+// const dosen = ['Prima', 'Rosa', 'Ando'];
+// const orang = [...mhs, ...dosen];
+// console.log(orang); 
+
+
+// Melalkukakn copy dalam array
+
+const mhs = ['Dherisma', 'Izza', 'Faiza'];
---
+++ spreadOperator.js
@@ -0,0 +1,8 @@
+<script>
+	function pengurangan (a, b, c) {
+	  console.log(a - b - c)
+	}
+ 
+	var nilai = [100, 25, 20]
+	pengurangan(...nilai) 
+</script>
